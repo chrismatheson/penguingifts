5675f7725d274b969c1e4d8c8f0178ca9ce9f724
Chris Matheson <chris.matheson@ivendi.com>
2013-07-15 08:32:03 +0100

    modifications to builder page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,24 @@
 /*global angular*/
 angular.module("builder", [])
 
-.controller("gridController", ["$scope", function ($scope) {
-   $scope.letters = [
-        ["","","","","","","","","",""],
-        ["","","","","f","","","","",""],
-        ["","","","","a","","","","",""],
-        ["","m","u","m","m","y","","","",""],
-        ["","","","","i","","","","",""],
-        ["","","","","l","","","","",""],
-        ["d","a","d","d","y","","","","",""],
-        ["","","","","","","","","",""],
-      ];
-}]);
+    .controller("gridController", ["$scope", function ($scope) {
+        "use strict";
+        $scope.letters = [
+            ["", "", "", "", "", "", "", "", "", ""],
+            ["", "", "", "", "f", "", "", "", "", ""],
+            ["", "", "", "", "a", "", "", "", "", ""],
+            ["", "m", "u", "m", "m", "y", "", "", "", ""],
+            ["", "", "", "", "i", "", "", "", "", ""],
+            ["", "", "", "", "l", "", "", "", "", ""],
+            ["d", "a", "d", "d", "y", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", "", "", ""]
+        ];
+    }])
+    .controller("letterController", ["$scope", function ($scope) {
+        "use strict";
+        $scope.mode = "empty";
+        $scope.$watch("letter", function (newVal, oldVal) {
+            $scope.letter = newVal.toUpperCase();
+            $scope.mode = newVal === "" ? "empty" : "tile";
+        });
+    }]);
